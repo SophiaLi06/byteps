4c47f4714eeb976cb8c5fc4f019265b5bc271fe1
SophiaLi06 <minghaoli@g.harvard.edu>
2021-11-26 00:10:56 -0500

    add scripts for using compressor

---
+++ scripts_lmh/worker_1_compressor.sh
@@ -0,0 +1,8 @@
+export BYTEPS_THREADPOOL_SIZE=16
+export NVIDIA_VISIBLE_DEVICES=0,1,2,3
+export DMLC_WORKER_ID=1
+export DMLC_NUM_WORKER=2
+export DMLC_ROLE=worker
+export DMLC_NUM_SERVER=1
+export DMLC_PS_ROOT_URI=10.31.179.$1  # the scheduler IP
+export DMLC_PS_ROOT_PORT=1234 # the scheduler port
