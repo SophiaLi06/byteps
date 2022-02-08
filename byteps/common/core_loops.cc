97f53a322a2e78951952a6efb491e9fec58f0c01
SophiaLi06 <ml922@cornell.edu>
2022-02-07 19:52:53 -0500

    stop test_wrapper

--- byteps/common/core_loops.cc
+++ byteps/common/core_loops.cc
@@ -334,7 +334,7 @@ bool RunRootNcclLoopOnce() {
       //   //auto gpu_addr = (char *)(tensor->data()) + task->offset;
       //   BPS_LOG(INFO) << "RootNccl Tensor GPU Addr: " << tensor->data() << "offset: " << task->offset << "\n";
       // }
-      test_wrapper(task->gpu_ptr, task->tensor->data(), task->offset);
+      //test_wrapper(task->gpu_ptr, task->tensor->data(), task->offset);
       /////////////
       tasks.push_back(task);
       queues.push_back(q);
@@ -408,7 +408,7 @@ bool RunNonRootNcclLoopOnce() {
     //   //auto gpu_addr = (char *)(tensor->data()) + task->offset;
     //   BPS_LOG(INFO) << "NonRootNccl Tensor GPU Addr: " << tensor->data() << "offset: " << task->offset << "\n";
     // }
-    test_wrapper(task->gpu_ptr, task->tensor->data(), task->offset);
+    //test_wrapper(task->gpu_ptr, task->tensor->data(), task->offset);
     //////////////
 
     tasks.push_back(task);
