bb4cf371f51d225605b8ecbf912597acecc3767e
SophiaLi06 <ml922@cornell.edu>
2022-02-13 17:29:57 -0500

    mxnet setup scripts

--- scripts_lmh/mxnet1.8_setup.sh
+++ scripts_lmh/mxnet1.8_setup.sh
@@ -5,7 +5,7 @@ module load gcc/4.9.3-fasrc01
 export BYTEPS_NCCL_HOME=/n/home03/minghaoli/.conda/pkgs/nccl-2.4.7.1-h51cf6c1_0
 export BYTEPS_NUMA_ON=0
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/n/home03/minghaoli/.conda/pkgs/nccl-2.4.7.1-h51cf6c1_0/lib
-LIBRARY_PATH=$LIBRARY_PATH:/n/home03/minghaoli/.conda/pkgs/nccl-2.4.7.1-h51cf6c1_0/lib
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/n/home03/minghaoli/.conda/pkgs/nccl-2.4.7.1-h51cf6c1_0/lib:/n/home03/minghaoli/byteps/byteps/common/test
+LIBRARY_PATH=$LIBRARY_PATH:/n/home03/minghaoli/.conda/pkgs/nccl-2.4.7.1-h51cf6c1_0/lib:/n/home03/minghaoli/byteps/byteps/common/test
 
 source activate mxnet1.8_cuda11_0
