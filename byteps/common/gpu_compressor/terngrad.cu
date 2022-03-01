0fcba4176528458795114548442f823d565a7d3a
SophiaLi06 <ml922@cornell.edu>
2022-03-01 01:34:05 -0500

    grad_max based on len

--- byteps/common/gpu_compressor/terngrad.cu
+++ byteps/common/gpu_compressor/terngrad.cu
@@ -86,7 +86,7 @@ __global__ void terngrad_compress_kernel(const void* gpu_ptr, size_t len, curand
 }
 
 void terngrad_compress(const void* gpu_ptr, size_t len){
-    std::cout << "compress len: " << len << std::endl;
+    //std::cout << "compress len: " << len << std::endl;
 #ifdef TOTAL_TIME_CUDA
     // Create the timer
     cudaEvent_t total_start, total_stop;
@@ -99,15 +99,15 @@ void terngrad_compress(const void* gpu_ptr, size_t len){
     float* ptr = reinterpret_cast<float*>(const_cast<void*>(gpu_ptr));
     
     float grad_max = 0.0;
-    float *host_max_res, *dev_max_res;
+    // float *host_max_res, *dev_max_res;
 
-    const unsigned int maxBlockCount = 32;
-    const unsigned int maxThreadPerBlock = 128;
+    // const unsigned int maxBlockCount = 32;
+    // const unsigned int maxThreadPerBlock = 128;
     
-    // Allocate space for result on host
-    host_max_res = (float*)calloc(maxBlockCount, sizeof(float));
-    // Allocate space for result on device
-    cudaMalloc(&dev_max_res, maxBlockCount * sizeof(float));
+    // // Allocate space for result on host
+    // host_max_res = (float*)calloc(maxBlockCount, sizeof(float));
+    // // Allocate space for result on device
+    // cudaMalloc(&dev_max_res, maxBlockCount * sizeof(float));
 
 #ifdef TIME_CUDA
     // Create the timer
@@ -118,18 +118,37 @@ void terngrad_compress(const void* gpu_ptr, size_t len){
     // Start the timer
     cudaEventRecord(start, 0);
 #endif
+    if (len > 200){
+        float *host_max_res, *dev_max_res;
+
+        const unsigned int maxBlockCount = 32;
+        const unsigned int maxThreadPerBlock = 128;
     
-    para_max<<<maxBlockCount, maxThreadPerBlock, maxThreadPerBlock * sizeof(float)>>>(gpu_ptr, len, dev_max_res);
+        // Allocate space for result on host
+        host_max_res = (float*)calloc(maxBlockCount, sizeof(float));
+        // Allocate space for result on device
+        cudaMalloc(&dev_max_res, maxBlockCount * sizeof(float));
+        para_max<<<maxBlockCount, maxThreadPerBlock, maxThreadPerBlock * sizeof(float)>>>(gpu_ptr, len, dev_max_res);
 
-    // Copy device result to host
-    cudaMemcpy(host_max_res, dev_max_res, maxBlockCount * sizeof(float), cudaMemcpyDeviceToHost);
-    cudaFree(dev_max_res);
+        // Copy device result to host
+        cudaMemcpy(host_max_res, dev_max_res, maxBlockCount * sizeof(float), cudaMemcpyDeviceToHost);
+        cudaFree(dev_max_res);
 
-    // Find the maximum value across all blocks
-    for (int i = 0; i < maxBlockCount; ++i){
-        if (host_max_res[i] > grad_max) grad_max = host_max_res[i];
+        // Find the maximum value across all blocks
+        for (int i = 0; i < maxBlockCount; ++i){
+            if (host_max_res[i] > grad_max) grad_max = host_max_res[i];
+        }
     }
-    std::cout << "Done grad_max finding" << std::endl;
+    else{
+        float* grad_max_answer;
+        cudaMalloc(&grad_max_answer, sizeof(float));
+        //find_grad_max<<<64, 64>>>(gpu_ptr, len, grad_max_answer);
+        find_grad_max<<<1, 1>>>(gpu_ptr, len, grad_max_answer);
+        cudaMemcpy(&grad_max, grad_max_answer, sizeof(float), cudaMemcpyDeviceToHost);
+        cudaFree(grad_max_answer);
+    }
+    
+    //std::cout << "Done grad_max finding" << std::endl;
 
 #ifdef TIME_CUDA
     // Stop the timer
