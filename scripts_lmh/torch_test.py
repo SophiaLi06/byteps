e5dc716467bfab5570716caaa3b078df078bf944
SophiaLi06 <minghaoli@g.harvard.edu>
2022-03-01 08:24:07 -0500

    torch test script

---
+++ scripts_lmh/torch_test.py
@@ -0,0 +1,18 @@
+import torch
+import time
+
+x = torch.rand(100000)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+gpu_tensor = x.to(device)
+start = time.time()
+b = torch.bernoulli(gpu_tensor)
+end = time.time()
+print(end - start)
+print(start, end)
+
+start1 = time.time()
+for i in range(100000):
+    torch.bernoulli(gpu_tensor[i])
+print(time.time() - start1)
