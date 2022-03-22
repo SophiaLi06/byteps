07b07ff6c5e5599dbaac097225ed15b3cda49982
SophiaLi06 <ml922@cornell.edu>
2022-03-22 16:30:48 -0400

    cleaning terngrad kernel

--- byteps/common/gpu_compressor/terngrad.cu
+++ byteps/common/gpu_compressor/terngrad.cu
@@ -85,8 +85,7 @@ __global__ void terngrad_compress_kernel(const void* gpu_ptr, size_t len, curand
     // TODO: change data type from float to uint8
 }
 
-void terngrad_compress1(const void* gpu_ptr, size_t len){
-    //std::cout << "compress len: " << len << std::endl;
+void terngrad_compress(const void* gpu_ptr, size_t len){
 #ifdef TOTAL_TIME_CUDA
     // Create the timer
     cudaEvent_t total_start, total_stop;
@@ -99,15 +98,6 @@ void terngrad_compress1(const void* gpu_ptr, size_t len){
     float* ptr = reinterpret_cast<float*>(const_cast<void*>(gpu_ptr));
     
     float grad_max = 0.0;
-    // float *host_max_res, *dev_max_res;
-
-    // const unsigned int maxBlockCount = 32;
-    // const unsigned int maxThreadPerBlock = 128;
-    
-    // // Allocate space for result on host
-    // host_max_res = (float*)calloc(maxBlockCount, sizeof(float));
-    // // Allocate space for result on device
-    // cudaMalloc(&dev_max_res, maxBlockCount * sizeof(float));
 
 #ifdef TIME_CUDA
     // Create the timer
@@ -147,8 +137,6 @@ void terngrad_compress1(const void* gpu_ptr, size_t len){
         cudaMemcpy(&grad_max, grad_max_answer, sizeof(float), cudaMemcpyDeviceToHost);
         cudaFree(grad_max_answer);
     }
-    
-    //std::cout << "Done grad_max finding" << std::endl;
 
 #ifdef TIME_CUDA
     // Stop the timer
@@ -158,23 +146,16 @@ void terngrad_compress1(const void* gpu_ptr, size_t len){
     cudaEventElapsedTime(&find_max_time, start, stop);
     std::cout << "Time to find grad_max: " << find_max_time << std::endl;
 #endif
-    // float grad_max;
-    // float* grad_max_answer;
-    // cudaMalloc(&grad_max_answer, sizeof(float));
-    // find_grad_max<<<64, 64>>>(gpu_ptr, len, grad_max_answer);
-    // //find_grad_max<<<1, 1>>>(gpu_ptr, len, grad_max_answer);
-    // cudaMemcpy(&grad_max, grad_max_answer, sizeof(float), cudaMemcpyDeviceToHost);
-    // cudaFree(grad_max_answer);
-    //std::cout << "grad_max: " << grad_max << std::endl;
 
     const unsigned int threadsPerBlock = 512;
-    // TODO: first try one block, then increase block number
     //const unsigned int blockCount = 64;
     const unsigned int blockCount = (len + threadsPerBlock - 1) / threadsPerBlock;
     const unsigned int totalThreads = threadsPerBlock * blockCount;
     curandState *devStates;
+
     // /* Allocate space for results on host */
     // hostResults = (unsigned int *)calloc(totalThreads, sizeof(int));
+
     /* Allocate space for prng states on device */
     cudaMalloc((void**)&devStates, totalThreads * sizeof(curandState));
     //std::cout << "Done mallocing for devStates" << std::endl;
@@ -185,9 +166,9 @@ void terngrad_compress1(const void* gpu_ptr, size_t len){
 #endif
     /* Setup prng states */
     setup_kernel<<<blockCount, threadsPerBlock>>>(devStates);
-    //std::cout << "Done setup" << std::endl;
+
     terngrad_compress_kernel<<<blockCount, threadsPerBlock>>>(gpu_ptr, len, devStates, grad_max);
-    //std::cout << "DOne compress" << std::endl;
+
 #ifdef TIME_CUDA
     // Stop the timer
     cudaEventRecord(stop, 0);
@@ -216,14 +197,8 @@ void terngrad_compress1(const void* gpu_ptr, size_t len){
 #endif
 }
 
-void terngrad_compress(const void* gpu_ptr, size_t len){
-    // TODO: placeholder, remove later!!!
-    return;
-}
-
 void terngrad_decompress(const void* gpu_ptr, float scale, size_t len){
     // TODO: time the gradient with a scale
-    // For now, just do nothing as I haven't figured out where to put scale
-    //std::cout << "Done decompress" << std::endl;
+
     return;
 }
