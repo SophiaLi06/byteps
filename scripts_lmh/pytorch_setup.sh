83d9cfbaa028ced72125256703f1106a289e04e0
SophiaLi06 <minghaoli@g.harvard.edu>
2022-09-16 16:56:46 -0400

    update pytorch setup script

--- scripts_lmh/pytorch_setup.sh
+++ scripts_lmh/pytorch_setup.sh
@@ -5,7 +5,7 @@ export BYTEPS_NCCL_HOME=/n/home03/minghaoli/.conda/pkgs/nccl-2.11.4.1-h97a9cb7_0
 
 export BYTEPS_NUMA_ON=0
 
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/n/home03/minghaoli/.conda/pkgs/nccl-2.11.4.1-h97a9cb7_0/lib:/n/home03/minghaoli/byteps/byteps/common/test
-LIBRARY_PATH=$LIBRARY_PATH:/n/home03/minghaoli/.conda/pkgs/nccl-2.11.4.1-h97a9cb7_0/lib:/n/home03/minghaoli/byteps/byteps/common/test
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/n/home03/minghaoli/.conda/pkgs/nccl-2.11.4.1-h97a9cb7_0/lib:/n/home03/minghaoli/byteps/byteps/common/test:/n/home03/minghaoli/byteps/byteps/common/gpu_compressor
+LIBRARY_PATH=$LIBRARY_PATH:/n/home03/minghaoli/.conda/pkgs/nccl-2.11.4.1-h97a9cb7_0/lib:/n/home03/minghaoli/byteps/byteps/common/test:/n/home03/minghaoli/byteps/byteps/common/gpu_compressor
 
 source activate pt1.8_cuda111
